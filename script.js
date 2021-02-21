d8cebb3fe82148988544a95564c582bda4276fdd
Rene Barrera <Carbonblack000@outlook.com>
2021-02-21 13:45:49 -0600

    just some initial testing

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+board = {rows:[[0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0]]};
 
+function display(){
+    let board = document.getElementById('board');
+    let tmp = makeElement('p','test');
+    board.append(tmp);
+}
+
+function makeElement(type, text){
+    let elem = document.createElement(type);
+    let theText = document.createTextNode(text);
+    elem.appendChild(theText);
+    return elem;
+}
