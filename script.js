9c46f3423d4fa00865710230e552cc06e963868b
Rene Barrera <Carbonblack000@outlook.com>
2021-02-28 15:29:06 -0600

    Added the ability to insert play chips, all that's missing is formatting and win state detection

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
-board = {rows:[[0,0,0,0,0,0,0],
+var board = [
         [0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0]]};
-
-function display(){
-    let board = document.getElementById('board');
-    let tmp = makeElement('p','test');
-    board.append(tmp);
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0]
+    ];
+turn = 1;
+function sayHi(id){
+    document.getElementById(id).style.backgroundColor = "red";
+    console.log(id.className);
 }
 
-function makeElement(type, text){
-    let elem = document.createElement(type);
-    let theText = document.createTextNode(text);
-    elem.appendChild(theText);
-    return elem;
+function insertAt(id){
+    let column = Number(id.charAt(1));
+    let i = -1;
+    for(i = 0; i < board.length; i++){
+        if(board[i][column] != 0){
+            break;
+        }
+    }
+    i--;
+    if(i != -1){
+        board[i][column] = turn;
+        turn *= -1;
+        changeColor(i+""+column, turn);
+    } else {
+        console.log("Invalid, column is full");
+    }
+}
+
+function changeColor(id, whichTurn){
+    if(whichTurn == 1){
+        document.getElementById(id).style.backgroundColor = "red";
+    }
+    if(whichTurn == -1){
+        document.getElementById(id).style.backgroundColor = "yellow";
+    }
 }
