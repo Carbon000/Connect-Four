699d9db81b76f35a2e3cc9910dfdf43126f2a2c6
Rene Barrera <Carbonblack000@outlook.com>
2021-03-29 14:31:06 -0500

    Added seed input text box and files needed for Heroku

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 app.use(express.static("client-side"));
 app.use(express.json());
-app.listen(3000,()=> {console.log("Running....")} );
+const PORT = process.env.PORT || 3000
+app.listen(PORT,()=> {console.log("Running....")} );
 
 
 
